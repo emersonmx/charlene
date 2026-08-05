4ff307fbf843c50c34c11ebd8c61056e9abab5b1
Emerson MX <emersonmx@gmail.com>
2026-08-05 13:41:15 -0300

    chore: update eslint config

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -11,12 +11,17 @@ export default defineConfig([
         files: ["**/*.{ts,tsx}"],
         extends: [
             js.configs.recommended,
-            tseslint.configs.recommended,
+            tseslint.configs.strictTypeChecked,
+            tseslint.configs.stylisticTypeChecked,
             reactHooks.configs.flat.recommended,
             reactRefresh.configs.vite,
         ],
         languageOptions: {
             globals: globals.browser,
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
         },
     },
     {
